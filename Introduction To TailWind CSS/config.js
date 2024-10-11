0dfa67a6fc61c2fe449d710385abfac96444c590
Ankur Verma <ankurverma7619@gmail.com>
2024-10-11 23:03:34 +0530

    Learning Gradients in Tailwind

---
+++ Introduction To TailWind CSS/config.js
@@ -0,0 +1,16 @@
+tailwind.config={
+    theme: {
+        extend: {
+            colors: {
+                my_teal: '#008080',
+                pale_orange:'#ffd9a6',
+                light_orange:'#fbb03b',
+                orange:'#f7931a',
+            },
+            // fontFamily: {
+            //     sans:[Bebas_Neue, 'sans-serif'],
+            //     slab:['Roboto Slab', 'serif']
+            // }
+        }
+    }
+}
